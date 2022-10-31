2591e5687da125d4753100f25572054cb0d9f837
Dmitriy <marinenko96@yandex.ru>
2022-10-31 19:30:53 +0500

    Factory method add

---
+++ Фабричный Метод/index.php
@@ -0,0 +1,49 @@
+<?php
+
+//PhoneInterface.php
+interface Phone
+{
+    public function call();
+}
+
+//PhoneFactory.php
+class PhoneFactory
+{
+    public function createCellPhone(): Phone
+    {
+        return new CellPhone();
+    }
+
+    public function createSmartPhone(): Phone
+    {
+        return new SmartPhone();
+    }
+}
+
+//CellPhone.php
+class CellPhone implements Phone
+{
+    public function call()
+    {
+        echo 'call on Cell Phone';
+    }
+}
+
+//SmartPhone.php
+class SmartPhone implements Phone
+{
+    public function call()
+    {
+        echo 'call on Smart Phone';
+    }
+}
+
+//index.php
+//include ("PhoneInterface.php");
+//include ("CellPhone.php");
+//include ("SmartPhone.php");
+//include ("PhoneFactory.php");
+
+$factory = new PhoneFactory();
+$cell_phone = $factory->createCellPhone();
+$smart_phone = $factory->createSmartPhone();
